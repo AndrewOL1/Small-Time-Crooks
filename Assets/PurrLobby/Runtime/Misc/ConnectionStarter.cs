1ece1474762837c03ce2bf17d1ecb09fcd40af4f
AndrewOL1 <aoleary7@student.fitchburgstate.edu>
2025-09-24 17:48:43 -0400

    ConnectionStarter

--- Assets/PurrLobby/Runtime/Misc/ConnectionStarter.cs
+++ Assets/PurrLobby/Runtime/Misc/ConnectionStarter.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using PurrNet;
 using PurrNet.Logging;
+using PurrNet.Steam;
 using PurrNet.Transports;
+using Steamworks;
 using UnityEngine;
 
 #if UTP_LOBBYRELAY
@@ -15,16 +17,31 @@ namespace PurrLobby
     {
         private NetworkManager _networkManager;
         private LobbyDataHolder _lobbyDataHolder;
+        private SteamTransport _steamTransport;
+        private UDPTransport _udpTransport;
+
+        private bool _isFromLobby;
         
         private void Awake()
         {
+            if (!TryGetComponent(out _steamTransport))
+            {
+                PurrLogger.LogError($"Failed to get {nameof(SteamTransport)} component.",this);
+            }
+            if (!TryGetComponent(out _udpTransport))
+            {
+                PurrLogger.LogError($"Failed to get {nameof(UDPTransport)} component.",this);
+            }
             if(!TryGetComponent(out _networkManager)) {
                 PurrLogger.LogError($"Failed to get {nameof(NetworkManager)} component.", this);
             }
             
             _lobbyDataHolder = FindFirstObjectByType<LobbyDataHolder>();
+            
             if(!_lobbyDataHolder)
                 PurrLogger.LogError($"Failed to get {nameof(LobbyDataHolder)} component.", this);
+            else
+                _isFromLobby = true;
         }
 
         private void Start()
@@ -34,22 +51,21 @@ namespace PurrLobby
                 PurrLogger.LogError($"Failed to start connection. {nameof(NetworkManager)} is null!", this);
                 return;
             }
-            
-            if (!_lobbyDataHolder)
+
+            if (!_steamTransport)
             {
-                PurrLogger.LogError($"Failed to start connection. {nameof(LobbyDataHolder)} is null!", this);
-                return;
-            }
-            
-            if (!_lobbyDataHolder.CurrentLobby.IsValid)
-            {
-                PurrLogger.LogError($"Failed to start connection. Lobby is invalid!", this);
+                PurrLogger.LogError($"Failed to start connection. {nameof(SteamTransport)} is null!", this);
                 return;
             }
 
             if(_networkManager.transport is PurrTransport) {
                 (_networkManager.transport as PurrTransport).roomName = _lobbyDataHolder.CurrentLobby.LobbyId;
-            } 
+            }
+
+            if (_isFromLobby)
+            {
+                StartFromLobby();
+            }
             
 #if UTP_LOBBYRELAY
             else if(_networkManager.transport is UTPTransport) {
@@ -65,6 +81,36 @@ namespace PurrLobby
             if(_lobbyDataHolder.CurrentLobby.IsOwner)
                 _networkManager.StartServer();
             StartCoroutine(StartClient());
+        }
+
+        private void StartFromLobby()
+        {
+            _networkManager.transport = _steamTransport;
+            if (!_lobbyDataHolder)
+            {
+                PurrLogger.LogError($"Failed to start connection. {nameof(LobbyDataHolder)} is null!", this);
+                return;
+            }
+            if (!_lobbyDataHolder.CurrentLobby.IsValid)
+            {
+                PurrLogger.LogError($"Failed to start connection. Lobby is invalid!", this);
+                return;
+            }
+
+            if (!ulong.TryParse(_lobbyDataHolder.CurrentLobby.LobbyId, out ulong ulobbyId))
+            {
+                Debug.LogError($"Failed to parse lobbyid into ulong", this);
+                return;
+            }
+            
+            var lobbyOwner = SteamMatchmaking.GetLobbyOwner(new CSteamID(ulobbyId));
+            if (!lobbyOwner.IsValid())
+            {
+                Debug.LogError($"Failed to get looby owner from parsed lobby ID", this);
+                return;
+            }
+
+            _steamTransport.address = lobbyOwner.ToString();
         }
 
         private IEnumerator StartClient()
