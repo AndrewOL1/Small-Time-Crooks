67186964dc608262afccb02f89a155d3851792c7
Kdog9001 <kdog9001g@gmail.com>
2025-10-15 11:09:25 -0400

    Unfinished button script

--- Assets/Scripts/CodeNum.cs
+++ Assets/Scripts/CodeNum.cs
@@ -3,15 +3,41 @@ using UnityEngine;
 public class CodeNum : MonoBehaviour
 {
     public string codeString;
+    public Transform originalPOS;
+    public Transform endPOS;
+    public float duration;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        originalPOS = gameObject.transform;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private void OnColliderEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("VR Hand"))
+        {
+            //StartCoroutine(pressButton());
+        }
+    }
+
+
+   /* IEnumerator pressButton()
+    {
+        float startTime = Time.time;
+        while (startTime < duration)
+        {
+            float fracComplete = (Time.time - startTime) / duration;
+            gameobject.transform.position = Vector3.Lerp(originalPOS, endPOS, fracComplete);
+            yield return null;
+        }
+    }*/
+
+
 }
