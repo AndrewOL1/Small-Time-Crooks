3fe00ab70ef5332e0bf736c1856161960dcac475
Kdog9001 <146856471+Kdog9001@users.noreply.github.com>
2025-10-28 16:09:16 -0400

    Button work

--- Assets/Scripts/CodeNum.cs
+++ Assets/Scripts/CodeNum.cs
@@ -7,18 +7,25 @@ public class CodeNum : MonoBehaviour
     public Transform originalPOS;
     public Transform endPOS;
     public float duration;
+
+    public bool buttonPressed; // a test bool
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        buttonPressed = false;
         originalPOS = gameObject.transform;
-        StartCoroutine(pressButton());
+        //StartCoroutine(pressButton());
         
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        posCorrecter();
+        if (buttonPressed)
+        {
+            StartCoroutine(pressButton());
+        }
     }
 
     private void OnColliderEnter(Collision other)
@@ -41,5 +48,28 @@ public class CodeNum : MonoBehaviour
         }
     }
 
+    IEnumerator unpressButton()
+    {
+        float startTime = Time.time;
+        while (startTime < duration)
+        {
+            float fracComplete = (Time.time - startTime) / duration;
+            gameObject.transform.position = Vector3.Lerp(endPOS.position, originalPOS.position, fracComplete);
+            yield return null;
+        }
+    }
+
+    public void posCorrecter()
+    {
+        if (gameObject.transform.position.x > originalPOS.position.x)
+        {
+            gameObject.transform.position = originalPOS.position;
+        }
+
+        if (gameObject.transform.position.x < endPOS.position.x)
+        {
+            gameObject.transform.position = endPOS.position;
+        }
+    }
 
 }
