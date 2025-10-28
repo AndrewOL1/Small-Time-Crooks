d7987215574dde34f16d1e1cb09e00a7e1adcbd3
AndrewOL1 <aoleary7@student.fitchburgstate.edu>
2025-10-28 17:19:32 -0400

    Update TogglePlayer.cs

--- Assets/AO/Scripts/TogglePlayer.cs
+++ Assets/AO/Scripts/TogglePlayer.cs
@@ -19,17 +19,32 @@ namespace AO.Scripts
     
         }
 
+        protected override void OnSpawned(bool asServer)
+        {
+            Vr.onChanged += OnVrChange;
+        }
+        private void OnVrChange(bool newValue)
+        {
+            Debug.Log("SyncVar has changed to: " + newValue);
+        }
+        private void ChangeVr(bool vr)
+        {
+            //This will change or add a value to the dictionary
+            Vr.value = vr;
+        }
         public void Start()
         {
             NetworkIdentity identity = GetComponent<NetworkIdentity>();
             if (identity.isOwner) {
                 if (FindFirstObjectByType<LobbyDataHolder>().isVRPlayer)
                 {
+                    ChangeVr(true);
                     vrPlayer.SetActive(true);
                     Startup();
                 }
                 else
                 {
+                    ChangeVr(false);
                     pcPlayer.SetActive(true);
                 }
             }
